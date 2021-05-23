a1eae5a5c9b60fbfe812567d3e9bd81b2cb68bfd
Asim Saeed <zedge744@live.com>
2021-05-23 19:50:53 +0500

    context updated

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -1,7 +1,7 @@
 exports.handler = async(event,context)=>{
     return {
         statusCode: 200,
-        body : 'this is just a test for netlify to verify if my functions is still working'
+        body : 'need to reverify if netliy diployed'
     }
 }
 
